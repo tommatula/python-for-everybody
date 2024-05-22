84e20b936d7290ce7c3c6fbddad1ba1aa3fd143b
Tom Matula <164676813+tommatula@users.noreply.github.com>
2024-05-22 14:41:51 -0700

    Add files via upload

---
+++ roster/roster.py
@@ -0,0 +1,63 @@
+import json
+import sqlite3
+import os
+
+fpath = os.path.dirname(__file__)
+#print(fpath)
+#fname = fpath+'/many-many.csv'
+
+conn = sqlite3.connect(fpath+'/rosterdb.sqlite')
+cur = conn.cursor()
+
+# Do some setup
+cur.executescript('''
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Member;
+DROP TABLE IF EXISTS Course;
+
+CREATE TABLE User (
+    id     INTEGER PRIMARY KEY,
+    name   TEXT UNIQUE
+);
+
+CREATE TABLE Course (
+    id     INTEGER PRIMARY KEY,
+    title  TEXT UNIQUE
+);
+
+CREATE TABLE Member (
+    user_id     INTEGER,
+    course_id   INTEGER,
+    role        INTEGER,
+    PRIMARY KEY (user_id, course_id)
+)
+''')
+
+fname = input('Enter file name: ')
+if len(fname) < 1:
+    fname = fpath+'/roster_data.json'
+
+
+str_data = open(fname).read()
+json_data = json.loads(str_data)
+
+for entry in json_data:
+    name = entry[0]
+    title = entry[1]
+    role = entry[2]
+
+    cur.execute('''INSERT OR IGNORE INTO User (name)
+        VALUES ( ? )''', ( name, ) )
+    cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
+    user_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Course (title)
+        VALUES ( ? )''', ( title, ) )
+    cur.execute('SELECT id FROM Course WHERE title = ? ', (title, ))
+    course_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Member
+        (user_id, course_id,role) VALUES ( ?, ?, ? )''',
+        ( user_id, course_id, role ) )
+
+    conn.commit()
