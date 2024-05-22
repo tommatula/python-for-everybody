da5d59b56b7cc20b43b5dc7c2e86452dfdbd4046
Tom Matula <164676813+tommatula@users.noreply.github.com>
2024-05-22 14:40:17 -0700

    Add files via upload

---
+++ Spyder/spyder.py
@@ -0,0 +1,17 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = 'http://py4e-data.dr-chuck.net/known_by_Karys.html'
+for i in range(7):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    print(tags[17].contents)
+    url = tags[17].get('href', None)
+
